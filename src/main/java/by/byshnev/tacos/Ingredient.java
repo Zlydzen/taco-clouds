fb08d6d3686d37ab13b73a86b787c788d02df532
Mikita Byshniou <nick.byshnev@gmail.com>
2022-01-03 16:52:02 +0300

    add showDesignForm in DesignTacoController

--- src/main/java/by/byshnev/tacos/Ingredient.java
+++ src/main/java/by/byshnev/tacos/Ingredient.java
@@ -1,15 +1,19 @@
 package by.byshnev.tacos;
 
 
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
 
+@Data                       // create constructor, getters, setters
+@RequiredArgsConstructor
 public class Ingredient {
 
-private final String id;
-private final String name;
-private final Type type;
+    private final String id;
+    private final String name;
+    private final Type type;
 
-public static enum Type {
-    WRAP, PROTEIN, VEGGIES, CHEESE, SAUCE
-}
+    public static enum Type {
+        WRAP, PROTEIN, VEGGIES, CHEESE, SAUCE
+    }
 
 }
---
+++ src/main/java/by/byshnev/tacos/Taco.java
@@ -0,0 +1,10 @@
+package by.byshnev.tacos;
+
+import lombok.Data;
+import java.util.List;
+
+@Data
+public class Taco {
+    private String name;
+    private List<String> ingredients;
+}
